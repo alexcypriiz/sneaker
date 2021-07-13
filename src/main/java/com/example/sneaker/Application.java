0064ba6ac063e9b36203d6472f48c1da80511d1b
alexcypriiz <alexey.podlubny@yandex.ru>
2021-07-14 00:11:25 +0400

    first commit

---
+++ src/main/java/com/example/sneaker/Application.java
@@ -0,0 +1,15 @@
+package com.example.sneaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Alexey Podlubnyy on 14.07.2021
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
