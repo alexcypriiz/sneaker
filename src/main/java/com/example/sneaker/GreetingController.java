dd1250562534e3e50727e6d47d50462e413ce434
alexcypriiz <alexey.podlubny@yandex.ru>
2021-07-17 23:30:14 +0400

    feat: add search messages by tags

--- src/main/java/com/example/sneaker/GreetingController.java
+++ src/main/java/com/example/sneaker/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -42,6 +43,18 @@ public class GreetingController {
 
         Iterable<Message> messages = messageRepository.findAll();
                 model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.put("messages", messages);
         return "main";
     }
 }
--- src/main/java/com/example/sneaker/repository/MessageRepository.java
+++ src/main/java/com/example/sneaker/repository/MessageRepository.java
@@ -3,8 +3,12 @@ package com.example.sneaker.repository;
 import com.example.sneaker.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Alexey Podlubnyy on 17.07.2021
  */
 public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
